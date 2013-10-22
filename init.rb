f76682be46defe3c5517ed7191ff1f36a3e842e0
Hisham Malik <hisham@arkhitech.com>
2013-10-22 20:13:47 +0500

    added plugin url settings

--- init.rb
+++ init.rb
@@ -1,6 +1,8 @@
 Redmine::Plugin.register :redmine_update_reminder do
   name 'Redmine Update Reminder'
   author 'Arkhitech'
+  url 'http://github.com/arkhitech/redmine_timesheet_plugin'
+  author_url 'https://github.com/arkhitech'
   description 'This is a plugin for Redmine which sends a reminder email to the assignee workign on a task, whose status is not updated with-in allowed duration'
   version '0.0.1'
   settings(default: {
