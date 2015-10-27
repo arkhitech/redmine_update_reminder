47fd4b5b00ad51e8ba12a9610b93fdfbe17f0d52
Artur Trofimov <arturtr@gmail.com>
2015-10-27 20:09:30 +1000

    fix check for assigners_updated_on

--- test/unit/telegram_user_test.rb
+++ test/unit/telegram_user_test.rb
@@ -2,8 +2,26 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class TelegramUserTest < ActiveSupport::TestCase
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_name
+    telegram_user = TelegramUser.new first_name: 'Bob', last_name: 'Marley'
+    assert telegram_user.name, 'Bob Marley'
+
+    telegram_user.username = 'bob_marley'
+
+    assert_equal telegram_user.name, 'Bob Marley @bob_marley'
+  end
+
+  def test_activate
+    telegram_user = TelegramUser.create first_name: 'Bob', last_name: 'Marley'
+    assert_equal telegram_user.active, true
+
+    telegram_user.activate
+    assert_equal telegram_user.active, true
+
+    telegram_user.deactivate
+    assert_equal telegram_user.active, false
+
+    telegram_user.activate
+    assert_equal telegram_user.active, true
   end
 end
