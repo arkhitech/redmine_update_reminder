78bc317aba2b2acc4673b6f10d67b8eb41e9bbc0
Artur Trofimov <arturtr@gmail.com>
2015-10-28 00:24:03 +1000

    add protocol from settings

--- lib/intouch.rb
+++ lib/intouch.rb
@@ -123,7 +123,11 @@ module Intouch
   end
 
   def self.issue_url(issue_id)
-    URI::HTTP.build({host: Setting['host_name'],  path: "/issues/#{issue_id}"}).to_s
+    if Setting['protocol'] == 'https'
+      URI::HTTPS.build({host: Setting['host_name'],  path: "/issues/#{issue_id}"}).to_s
+    else
+      URI::HTTP.build({host: Setting['host_name'],  path: "/issues/#{issue_id}"}).to_s
+    end
   end
 
 end
