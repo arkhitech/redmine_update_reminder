994f8d31fc8df1913f81669eb4b669677447a3e7
Artur Trofimov <arturtr@gmail.com>
2015-12-28 17:07:52 +1000

    Add feature to remove telegram group chats

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 put 'intouch/save_settings' => 'intouch#save_settings'
 resources :settings_templates
 resources :telegram_users, only: [:edit, :update, :destroy]
+resources :telegram_groups, only: [:destroy]
 
 resources :sidekiq_cron_jobs do
   collection do
