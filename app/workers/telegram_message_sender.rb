e072be12a223d7d71cec3de8e3daa702b8acae1a
Artur Trofimov <arturtr@gmail.com>
2015-12-28 13:12:20 +1000

    Update error handling for message sender, add messages to logging

--- app/workers/telegram_message_sender.rb
+++ app/workers/telegram_message_sender.rb
@@ -2,34 +2,58 @@ class TelegramMessageSender
   include Sidekiq::Worker
   sidekiq_options queue: :telegram,
                   rate: {
-                      name: 'telegram_rate_limit',
-                      limit: 15,
-                      period: 1
+                    name: 'telegram_rate_limit',
+                    limit: 15,
+                    period: 1
                   }
 
   TELEGRAM_MESSAGE_SENDER_LOG = Logger.new(Rails.root.join('log/intouch', 'telegram-message-sender.log'))
+  TELEGRAM_MESSAGE_SENDER_ERRORS_LOG = Logger.new(Rails.root.join('log/intouch', 'telegram-message-sender-errors.log'))
 
   def perform(telegram_user_id, message)
     token = Setting.plugin_redmine_intouch['telegram_bot_token']
     bot = Telegrammer::Bot.new(token)
 
     begin
-      bot.send_message(chat_id: telegram_user_id, text: message, disable_web_page_preview: true, parse_mode: 'Markdown')
-    rescue Telegrammer::Errors::BadRequestError => e
-      TELEGRAM_MESSAGE_SENDER_LOG.info "MESSAGE: #{message}"
 
-      telegram_user = if telegram_user_id > 0
-                        TelegramUser.find_by tid: telegram_user_id
-                      else
-                        TelegramGroupChat.find_by tid: telegram_user_id.abs
-                      end
+      bot.send_message(chat_id: telegram_user_id, text: message, disable_web_page_preview: true, parse_mode: 'Markdown')
+      TELEGRAM_MESSAGE_SENDER_LOG.info "telegram_user_id: #{telegram_user_id}\tmessage: #{message}"
+
+    rescue Telegrammer::Errors::BadRequestError => e
+
+      TELEGRAM_MESSAGE_SENDER_ERRORS_LOG.info "MESSAGE: #{message}"
+
+      telegram_user = (telegram_user_id > 0) ?
+        TelegramUser.find_by(tid: telegram_user_id) :
+        TelegramGroupChat.find_by(tid: telegram_user_id.abs)
+
       if e.message.include? 'Bot was kicked'
+
         telegram_user.deactivate if telegram_user.is_a? TelegramUser
-        TELEGRAM_MESSAGE_SENDER_LOG.info "Bot was kicked from chat. Deactivate #{telegram_user.inspect}"
+        TELEGRAM_MESSAGE_SENDER_ERRORS_LOG.info "Bot was kicked from chat. Deactivate #{telegram_user.inspect}"
+
+      elsif e.message.include? '429' or e.message.include? 'retry later'
+
+        TELEGRAM_MESSAGE_SENDER_ERRORS_LOG.error "429 retry later error. retry to send after 5 seconds\ntelegram_user_id: #{telegram_user_id}\tmessage: #{message}"
+        TelegramMessageSender.perform_in(5.seconds, telegram_user_id, message)
+
       else
-        TELEGRAM_MESSAGE_SENDER_LOG.error "#{e.class}: #{e.message}"
-        TELEGRAM_MESSAGE_SENDER_LOG.debug "#{telegram_user.inspect}"
+
+        TELEGRAM_MESSAGE_SENDER_ERRORS_LOG.error "#{e.class}: #{e.message}"
+        TELEGRAM_MESSAGE_SENDER_ERRORS_LOG.debug "#{telegram_user.inspect}"
+
       end
+
+    rescue Telegrammer::Errors::ServiceUnavailableError
+
+      TELEGRAM_MESSAGE_SENDER_ERRORS_LOG.error "ServiceUnavailableError. retry to send after 5 seconds\ntelegram_user_id: #{telegram_user_id}\tmessage: #{message}"
+      TelegramMessageSender.perform_in(5.seconds, telegram_user_id, message)
+
+    rescue 	Telegrammer::Errors::TimeoutError
+
+      TELEGRAM_MESSAGE_SENDER_ERRORS_LOG.error "TimeoutError. retry to send after 5 seconds\ntelegram_user_id: #{telegram_user_id}\tmessage: #{message}"
+      TelegramMessageSender.perform_in(5.seconds, telegram_user_id, message)
+
     end
   end
 
