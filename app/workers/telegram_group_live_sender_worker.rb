f8860cae35f53219629292c9e4e8bfbe3157bdb2
Artur Trofimov <arturtr@gmail.com>
2016-06-09 15:07:56 +1000

    Add detailed log for live messages

--- app/workers/telegram_group_live_sender_worker.rb
+++ app/workers/telegram_group_live_sender_worker.rb
@@ -3,10 +3,14 @@ class TelegramGroupLiveSenderWorker
   TELEGRAM_GROUP_LIVE_SENDER_LOG = Logger.new(Rails.root.join('log/intouch', 'telegram-group-live-sender.log'))
 
   def perform(issue_id)
+    TELEGRAM_GROUP_LIVE_SENDER_LOG.debug "START for issue_id #{issue_id}"
     Intouch.set_locale
 
     issue = Issue.find issue_id
+    TELEGRAM_GROUP_LIVE_SENDER_LOG.debug issue.inspect
+
     telegram_groups_settings = issue.project.active_telegram_settings.try(:[], 'groups')
+    TELEGRAM_GROUP_LIVE_SENDER_LOG.debug "telegram_groups_settings: #{telegram_groups_settings.inspect}"
 
     return unless telegram_groups_settings.present?
 
@@ -14,28 +18,46 @@ class TelegramGroupLiveSenderWorker
       v.try(:[], issue.status_id.to_s).try(:include?, issue.priority_id.to_s)
     end.keys
 
+    TELEGRAM_GROUP_LIVE_SENDER_LOG.debug "group_ids: #{group_ids.inspect}"
+
     only_unassigned_group_ids = telegram_groups_settings.select { |k, v| v.try(:[], 'only_unassigned').present? }.keys
+
+    TELEGRAM_GROUP_LIVE_SENDER_LOG.debug "only_unassigned_group_ids: #{group_ids.inspect}"
 
     group_ids -= only_unassigned_group_ids unless issue.total_unassigned?
 
+    TELEGRAM_GROUP_LIVE_SENDER_LOG.debug "group_ids: #{group_ids.inspect} (total_unassigned? = #{issue.total_unassigned?.inspect})"
+
     group_for_send_ids = if issue.alarm? or Intouch.work_time?
+                           TELEGRAM_GROUP_LIVE_SENDER_LOG.debug 'Alarm or work time'
 
                            group_ids
 
                          else
+                           TELEGRAM_GROUP_LIVE_SENDER_LOG.debug 'Anytime notifications'
+
                            anytime_group_ids = telegram_groups_settings.select { |k, v| v.try(:[], 'anytime').present? }.keys
 
                            (group_ids & anytime_group_ids)
                          end
 
+    TELEGRAM_GROUP_LIVE_SENDER_LOG.debug "group_for_send_ids: #{group_for_send_ids.inspect}"
+
     return unless group_for_send_ids.present?
 
     message = issue.telegram_live_message
 
+    TELEGRAM_GROUP_LIVE_SENDER_LOG.debug "message: #{message}"
+
     TelegramGroupChat.where(id: group_for_send_ids).uniq.each do |group|
+      TELEGRAM_GROUP_LIVE_SENDER_LOG.debug "group: #{group.inspect}"
       next unless group.tid.present?
-      TelegramMessageSender.perform_async(-group.tid, message)
+
+      job = TelegramMessageSender.perform_async(-group.tid, message)
+
+      TELEGRAM_GROUP_LIVE_SENDER_LOG.debug job.inspect
     end
+    TELEGRAM_GROUP_LIVE_SENDER_LOG.debug "DONE for issue_id #{issue_id}"
   rescue ActiveRecord::RecordNotFound => e
     # ignore
   end
--- app/workers/telegram_live_sender_worker.rb
+++ app/workers/telegram_live_sender_worker.rb
@@ -3,14 +3,23 @@ class TelegramLiveSenderWorker
   TELEGRAM_LIVE_SENDER_LOG = Logger.new(Rails.root.join('log/intouch', 'telegram-live-sender.log'))
 
   def perform(issue_id)
+    TELEGRAM_LIVE_SENDER_LOG.debug "START for issue_id #{issue_id}"
+
     Intouch.set_locale
+
     issue = Issue.find issue_id
+    TELEGRAM_LIVE_SENDER_LOG.debug issue.inspect
 
     base_message = issue.telegram_live_message
 
+    TELEGRAM_LIVE_SENDER_LOG.debug "base_message: #{base_message}"
+
+
     issue.intouch_live_recipients('telegram').each do |user|
+      TELEGRAM_LIVE_SENDER_LOG.debug "user: #{user.inspect}"
 
       telegram_user = user.telegram_user
+      TELEGRAM_LIVE_SENDER_LOG.debug "telegram_user: #{telegram_user.inspect}"
       next unless telegram_user.present? and telegram_user.active?
 
       roles_in_issue = []
@@ -18,6 +27,8 @@ class TelegramLiveSenderWorker
       roles_in_issue << 'assigned_to' if issue.assigned_to_id == user.id
       roles_in_issue << 'watchers' if issue.watchers.pluck(:user_id).include? user.id
       roles_in_issue << 'author' if issue.author_id == user.id
+
+      TELEGRAM_LIVE_SENDER_LOG.debug "roles_in_issue: #{roles_in_issue.inspect}"
 
       project  = issue.project
       settings = project.active_telegram_settings
@@ -37,8 +48,14 @@ class TelegramLiveSenderWorker
 
       message = prefix.present? ? "#{prefix}\n#{base_message}" : base_message
 
-      TelegramMessageSender.perform_async(telegram_user.tid, message)
+      TELEGRAM_LIVE_SENDER_LOG.debug message
+
+      job = TelegramMessageSender.perform_async(telegram_user.tid, message)
+
+      TELEGRAM_LIVE_SENDER_LOG.debug job.inspect
     end
+
+    TELEGRAM_LIVE_SENDER_LOG.debug "FINISH for issue_id #{issue_id}"
   rescue ActiveRecord::RecordNotFound => e
     # ignore
   end
