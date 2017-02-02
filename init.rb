191bf17005a7201f383684813a7c78699a9f6252
Igor Olemskoi <igor@southbridge.ru>
2017-02-02 12:33:36 +0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -15,7 +15,7 @@ Redmine::Plugin.register :redmine_intouch do
   description 'This is a plugin for Redmine which sends a reminder email and Telegram messages to the assignee workign on a task, whose status is not updated with-in allowed duration'
   version '0.4.0'
   author 'Centos-admin.ru'
-  author_url 'http://centos-admin.ru'
+  author_url 'https://centos-admin.ru'
 
   requires_redmine version_or_higher: '3.0'
 
