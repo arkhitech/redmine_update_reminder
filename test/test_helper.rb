7274a185daf7410c7c675bb1873d9310944d601c
Artur Trofimov <arturtr@gmail.com>
2017-02-21 21:25:33 +1000

    Fix projects helper patch

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,6 @@ $VERBOSE = nil # for hide ruby warnings
 
 # Load the Redmine helper
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
-require "minitest/reporters"
+require 'minitest/reporters'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
