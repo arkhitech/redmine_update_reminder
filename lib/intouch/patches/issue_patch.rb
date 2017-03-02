fa0230e9a7484423cc836934d41e2171469d303e
cross <cross@vgg.ru>
2017-03-02 09:04:53 +0300

    Edit links.

--- lib/intouch/patches/issue_patch.rb
+++ lib/intouch/patches/issue_patch.rb
@@ -238,10 +238,10 @@ module Intouch
 
             message += "\n#{I18n.t('field_status')}: #{status.name}" unless updated_details.include?('status')
 
-            message += "\n[##{id} #{I18n.t('intouch.telegram_message.issue.link')}](#{Intouch.issue_url(id)})"
+            message += "\n#{Intouch.issue_url(id)}"
 
             if telegram_group.present? && telegram_group.shared_url.present?
-              message += " | [#{I18n.t('intouch.telegram_message.issue.telegram_link')}](#{telegram_group.shared_url})"
+              message += ", [#{I18n.t('intouch.telegram_message.issue.telegram_link')}](#{telegram_group.shared_url})"
             end
 
             message
