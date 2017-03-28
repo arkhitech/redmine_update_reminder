5d83034eadb3df679e01e962d645ab13ccf4c454
Artur Trofimov <arturtr@gmail.com>
2017-03-28 16:30:34 +1000

    Add logger

--- lib/intouch/live/checker/base.rb
+++ lib/intouch/live/checker/base.rb
@@ -3,9 +3,14 @@ module Intouch::Live::Checker
     attr_reader :project, :issue, :journal
 
     def initialize(issue:, project:, journal: nil)
-      @issue = issue.reload
+      @issue = issue
       @project = project
       @journal = journal
+
+      logger.info 'Initialized with:'
+      logger.debug "Issue: #{issue.inspect}"
+      logger.debug "Project: #{project.inspect}"
+      logger.debug "Journal: #{journal.inspect}"
     end
 
     def required?
@@ -13,17 +18,34 @@ module Intouch::Live::Checker
     end
 
     def project_enabled?
+      logger.info 'Project enabled?'
+
+      logger.debug "project.module_enabled?(:intouch) => #{project.module_enabled?(:intouch).inspect}"
+      logger.debug "project.active? => #{project.active?.inspect}"
+
       project.module_enabled?(:intouch) && project.active?
     end
 
     def issue_open?
+      logger.debug "!issue.closed? => #{!issue.closed?.inspect}"
+
       !issue.closed? || journal_issue_state_open?
     end
 
     def journal_issue_state_open?
+      logger.debug "journal.present? => #{journal.present?.inspect}"
       return false unless journal.present?
 
-      journal.reload.new_status && !journal.new_status&.is_closed?
+      logger.debug "journal.new_status => #{journal.new_status.inspect}"
+      logger.debug "!journal.new_status&.is_closed? => #{!journal.new_status&.is_closed?.inspect}"
+
+      journal.new_status && !journal.new_status&.is_closed?
+    end
+
+    private
+
+    def logger
+      @logger ||= Logger.new(Rails.root.join('log/intouch', 'live-checker-base.log'))
     end
   end
 end
