b9315b788c216a55bf1b050e59b1b7d41d9482fa
Vladislav Yashin <v.yashin.work@gmail.com>
2018-12-14 21:25:09 +0400

    Fix ProjectsHelperPatch

--- lib/intouch/patches/projects_helper_patch.rb
+++ lib/intouch/patches/projects_helper_patch.rb
@@ -4,24 +4,27 @@ module Intouch
   module Patches
     module ProjectsHelperPatch
       def self.included(base)
-        base.prepend MethodsPatch
+        base.send(:include, MethodsPatch)
 
         base.module_eval do
           unloadable
+
+          alias_method :project_settings_tabs_without_intouch, :project_settings_tabs
+          alias_method :project_settings_tabs, :project_settings_tabs_with_intouch
         end
       end
 
       module MethodsPatch
-        def project_settings_tabs
-          tabs = super
+        def project_settings_tabs_with_intouch
+          tabs = project_settings_tabs_without_intouch
 
           return tabs unless User.current.allowed_to?(:manage_intouch_settings, @project)
 
           tabs.push(
-            name: 'intouch_settings',
-            action: :manage_intouch_settings,
-            partial: 'projects/settings/intouch/settings',
-            label: 'intouch.label.settings'
+              name: 'intouch_settings',
+              action: :manage_intouch_settings,
+              partial: 'projects/settings/intouch/settings',
+              label: 'intouch.label.settings'
           )
 
           tabs
