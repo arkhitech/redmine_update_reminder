1d63fbfd90c743c372a2957e9985f1980413f2f6
Hisham Malik <hisham@arkhitech.com>
2021-09-13 03:20:51 +0500

    updated for Rails 4+ compatibility

--- app/models/reminding_mailer.rb
+++ app/models/reminding_mailer.rb
@@ -5,13 +5,38 @@ class RemindingMailer < ActionMailer::Base
   include Redmine::Utils::DateCalculation
 
   def self.default_url_options
-    Mailer.default_url_options
+    ::Mailer.default_url_options
   end
   
-  def cc_email_addresses
-    return '' if non_working_week_days.include?(Date.today.cwday)
-    cc_group = Setting.plugin_redmine_update_reminder['cc']
-    Group.includes(:users).find(cc_group).users.map(&:mail) if cc_group.present?
+  def cc_group_email_addresses
+    @cc_group_email_addresses ||= begin
+      cc_group = Setting.plugin_redmine_update_reminder['cc']
+      if cc_group.present?
+        Group.includes(:users).find(cc_group).users.map(&:mail) 
+      else
+        []
+      end
+  
+    end
+  end
+  private :cc_group_email_addresses
+
+  def cc_role_email_addresses(user)
+    cc_role_ids = Setting.plugin_redmine_update_reminder['cc_roles']
+    if cc_role_ids.present?
+      User.active.preload(:email_address).joins(members: :member_roles).where("#{Member.table_name}.project_id" => user.projects.pluck(:id)).
+        where("#{MemberRole.table_name}.role_id IN (?)", cc_role_ids).map(&:email_address).map(&:address)
+    else
+      []
+    end
+  end
+  private :cc_role_email_addresses
+
+  def cc_email_addresses(user)
+    return [] if non_working_week_days.include?(Date.today.cwday)
+    cc_email_addresses = cc_group_email_addresses
+    cc_email_addresses += cc_role_email_addresses(user)    
+    cc_email_addresses.uniq
   end
   private :cc_email_addresses
   
@@ -20,7 +45,7 @@ class RemindingMailer < ActionMailer::Base
     @issue = issue
     @updated_since = updated_since
         
-    mail(to: @user.mail, subject: @issue.subject, cc: cc_email_addresses)
+    mail(to: @user.mail, subject: @issue.subject, cc: cc_email_addresses(user))
   end
 
   def reminder_status_email(user, issue, updated_since)
@@ -28,42 +53,43 @@ class RemindingMailer < ActionMailer::Base
     @issue = issue
     @updated_since = updated_since
 
-    mail(to: @user.mail, subject: @issue.subject, cc: cc_email_addresses)
+    mail(to: user.mail, subject: @issue.subject, cc: cc_email_addresses(user))
   end
   
   def remind_user_issue_trackers(user, issues_with_updated_since)
     @user = user
     @issues_with_updated_since = issues_with_updated_since
-    
-    mail(to: @user.mail, subject: I18n.t('update_reminder.issue_tracker_update_required', 
-      user_name: user.name, issue_count: @issues_with_updated_since.count), cc: cc_email_addresses)
+    subject = I18n.t('update_reminder.issue_tracker_update_required', 
+      user_name: user.name, issue_count: @issues_with_updated_since.count)
+
+    mail(to: user.mail, subject: subject, cc: cc_email_addresses(user))
   end
 
   def remind_user_issue_statuses(user, issues_with_updated_since)
     @user = user
     @issues_with_updated_since = issues_with_updated_since
     
-    mail(to: @user.mail, subject: I18n.t('update_reminder.issue_status_update_required', 
-      user_name: user.name, issue_count: @issues_with_updated_since.count), 
-      cc: cc_email_addresses)
+    subject = I18n.t('update_reminder.issue_status_update_required', 
+      user_name: user.name, issue_count: @issues_with_updated_since.count)
+    mail(to: user.mail, subject: subject, cc: cc_email_addresses(user))
   end
 
   def remind_user_past_due_issues(user, issues)
     @user = user
     @issues = issues
     
-    mail(to: @user.mail, subject: I18n.t('update_reminder.past_due_issue_update_required', 
-      user_name: user.name, issue_count: @issues.count), 
-      cc: cc_email_addresses)
+    subject = I18n.t('update_reminder.past_due_issue_update_required', 
+      user_name: user.name, issue_count: @issues.count)
+    mail(to: user.mail, subject: subject, cc: cc_email_addresses(user))
   end
 
   def remind_user_issue_estimates(user, issues_with_updated_since)
     @user = user
     @issues_with_updated_since = issues_with_updated_since
     
-    mail(to: @user.mail, subject: I18n.t('update_reminder.issue_estimate_required', 
-      user_name: user.name, issue_count: @issues_with_updated_since.count), 
-      cc: cc_email_addresses)
+    subject = I18n.t('update_reminder.issue_estimate_required', 
+      user_name: user.name, issue_count: @issues_with_updated_since.count)
+    mail(to: user.mail, subject: subject, cc: cc_email_addresses(user))
   end
   
 end
--- init.rb
+++ init.rb
@@ -1,3 +1,8 @@
+Rails.configuration.to_prepare do
+  require_dependency 'user'  
+  User.send(:include, RedmineUpdateReminder::Patches::UserPatch)  
+end  
+
 Redmine::Plugin.register :redmine_update_reminder do
   name 'Redmine Update Reminder'
   author 'Arkhitech'
@@ -6,7 +11,7 @@ Redmine::Plugin.register :redmine_update_reminder do
   description 'This is a plugin for Redmine which sends a reminder email to the assignee working on a task, whose status is not updated with-in allowed duration'
   version '1.1'
   settings(default: {
-             'header' => 'Update on Task Required',
+             'header' => 'Missing Required Task Update',
              'footer' => 'powered by arkhitech.com',
            }, partial: 'settings/reminder_settings')
 end
